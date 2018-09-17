087ce09a233a0f1c5385ef684cffedc108d2735d
Shalom Yerushalmy <yershalom@gmail.com>
2018-09-17 16:25:08 +0300

    Add fab for toggle between grid layout to LinearLayout

--- app/src/main/java/com/yershalom/checktheidle/MainActivity.kt
+++ app/src/main/java/com/yershalom/checktheidle/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.yershalom.checktheidle
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.os.StrictMode
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.View
 import com.yershalom.checktheidle.adapters.TopicAdapter
 import com.yershalom.checktheidle.data.Model
 import com.yershalom.checktheidle.data.RedditApiService
@@ -14,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     private var topics: List<Model.ChildrenData>? = null
     private lateinit var gridLayoutManager: GridLayoutManager
-//    private val llm = LinearLayoutManager(this)
+    private val llm = LinearLayoutManager(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
@@ -28,6 +32,22 @@ class MainActivity : AppCompatActivity() {
 
         initializeData()
         initializeAdapter()
+
+        fab.setOnClickListener {
+            ObjectAnimator.ofFloat(fab, "rotation", 0f, 180f).setDuration(500).start()
+            toggleView()
+        }
+
+        rv_topic.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (dy > 0 && fab.visibility === View.VISIBLE) {
+                    fab.hide()
+                } else if (dy < 0 && fab.visibility !== View.VISIBLE) {
+                    fab.show()
+                }
+            }
+        })
     }
 
     private fun initializeData(){
@@ -43,5 +63,13 @@ class MainActivity : AppCompatActivity() {
     private fun initializeAdapter(){
         val adapter = TopicAdapter(this.topics!!)
         rv_topic.adapter = adapter
+    }
+
+    private fun toggleView() {
+        if (rv_topic.layoutManager == llm) {
+            rv_topic.layoutManager = gridLayoutManager
+        } else {
+            rv_topic.layoutManager = llm
+        }
     }
 }
